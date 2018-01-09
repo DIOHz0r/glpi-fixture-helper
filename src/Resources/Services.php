854283eac8e2bdc056164834600a44cf3f774b95
Domingo Oropeza <doropeza@teclib.com>
2018-01-09 17:12:20 -0400

    feat(services): added definition of services for the container

---
+++ src/Resources/Services.php
@@ -0,0 +1,17 @@
+<?php
+
+use function DI\object;
+use DIOHz0r\Glpi\Fixtures\Command\FixtureLoadCommand;
+use DIOHz0r\Glpi\Fixtures\DatabaseManager;
+use DIOHz0r\Glpi\Fixtures\FixtureLocator;
+use DIOHz0r\Glpi\Fixtures\Interfaces\DatabaseManagerInterface;
+use DIOHz0r\Glpi\Fixtures\Interfaces\FixtureLocatorInterface;
+use DIOHz0r\Glpi\Fixtures\Interfaces\LoaderInterface;
+use DIOHz0r\Glpi\Fixtures\Loader;
+
+return [
+   FixtureLoadCommand::class => object()->constructorParameter('name','glpi:fixtures:load'),
+   DatabaseManagerInterface::class => object(DatabaseManager::class),
+   LoaderInterface::class => object(Loader::class),
+   FixtureLocatorInterface::class => object(FixtureLocator::class),
+];
